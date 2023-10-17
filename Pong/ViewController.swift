21f8a261aa6d376314b50bcf85553f97f0def14a
santranti <145852753+santranti@users.noreply.github.com>
2023-10-17 13:15:30 +0200

    Initial Commit

---
+++ Pong/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Pong
+//
+//  Created by Federico Calabró on 17/10/23.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
